af8f7414918c7629f928d1a3366871406f469672
iRaYzEn <abbassgame335463@gmail.com>
2024-01-14 21:35:15 +0300

    fixing accessibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,7 @@ export default function Header({ onClick }) {
       <div className="wrapper wrapper--header">
         <h1 className="header__title">Where in the world?</h1>
         <div role="button" onClick={onClick} className="header__theme">
-          <button
-            aria-label="btn-theme"
-            className="btn btn--theme fa-solid fa-moon"
-          ></button>
+          <i className="fa-solid fa-moon btn btn--theme"></i>
           <p className="dark-mode">Dark Mode</p>
         </div>
       </div>
